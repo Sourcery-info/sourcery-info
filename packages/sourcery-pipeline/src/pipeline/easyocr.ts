528d967d26832dffe67defd76a6277e06a4c92e2
Jason <jason@10layer.com>
2024-12-13 16:33:55 +0100

    Fix up easyocr

--- packages/sourcery-pipeline/src/pipeline/easyocr.ts
+++ packages/sourcery-pipeline/src/pipeline/easyocr.ts
@@ -1,14 +1,14 @@
 import { PipelineBase } from "./base"
-import { File } from "@sourcery/common/src/file";
 import path from "path";
-import { PDFiumLibrary } from '@hyzyla/pdfium';
+import fs from "fs/promises";
 import { execCommand } from "../execCommand";
 import type { SourceryFile } from "@sourcery/common/types/SourceryFile.type";
+import { isValidImage } from "@sourcery/common/src/utils";
 
 export class EasyOCRPipeline extends PipelineBase {
 
     constructor(file: SourceryFile) {
-        super(file, "md");
+        super(file, "json");
         console.log("EasyOCR Pipeline constructor");
     }
     
@@ -18,8 +18,13 @@ export class EasyOCRPipeline extends PipelineBase {
             let x = 0;
             for (const image of images) {
                 console.log(`Processing ${image}`);
+                const imagePath = path.join(this.filepath, "images", image);
+                if (!(await isValidImage(imagePath))) {
+                    console.error(`Invalid or unsupported image file: ${image}`);
+                    continue;
+                }
                 // const cmd = `/app/sourcery-ocr-env/bin/easyocr -l en -f ${path.join(this.filepath, this.stage_name, "images", image)} > ${path.join(this.filepath, this.stage_name, "results", image)}`;
-                const cmd = `/app/sourcery-ocr-env/bin/easyocr -l en -f ${path.join(this.filepath, "images", image)} --model_storage_directory /app/packages/sourcery-frontend/assets/easyocr/ > ${path.join(this.filepath, "easyocr", this.file.filename)}.${x++}.txt`;
+                const cmd = `/app/sourcery-ocr-env/bin/easyocr --paragraph True --output_format json -l en -f ${imagePath}  --model_storage_directory /app/packages/sourcery-frontend/assets/easyocr/ > ${path.join(this.filepath, "easyocr", this.file.filename)}.${x++}.jsonp`;
                 console.log(cmd);
                 const result = await execCommand(cmd);
                 console.log(result);
@@ -28,19 +33,24 @@ export class EasyOCRPipeline extends PipelineBase {
             console.error(error);
             throw error;
         }
-        // console.log(`pdftocairo ${this.last_filename} -png ${path.join(this.filepath, this.stage_name, "images")}`);
-        // await exec(`pdftocairo ${this.last_filename} -png ${path.join(this.filepath, this.stage_name, "images")}`);
-        // console.log(`/app/sourcery-ocr-env/bin/easyocr ${path.join(this.filepath, this.stage_name, "images")} ${path.join(this.filepath, this.stage_name, "results")}`);
-        // await exec(`/app/sourcery-ocr-env/bin/easyocr -l en ${path.join(this.filepath, this.stage_name, "images")} ${path.join(this.filepath, this.stage_name, "results")}`);
-        // const result = await fetch(`http://sourcery-ocr:3000/ocr`, {
-        //     method: 'POST',
-        //     body: JSON.stringify({
-        //         project: this.file.project,
-        //         filename: this.file.filename
-        //     })
-        // });
-        // const data = await result.json();
-        // console.log(data);
+        const files = await fs.readdir(path.join(this.filepath, "easyocr"));
+        let pages = [];
+        let x = 0;
+        for (const file of files) {
+            try {
+                const raw = await fs.readFile(path.join(this.filepath, "easyocr", file), "utf-8");
+                const data = raw.split("\n").filter(line => line.trim() !== "").map(line => JSON.parse(line));
+                pages.push({
+                    page: x++,
+                    filename: file,
+                    data: data
+                });
+            } catch (error) {
+                console.error(error);
+            }
+        }
+        console.log(pages);
+        await fs.writeFile(path.join(this.filepath, "easyocr", `${this.file.filename}.json`), JSON.stringify(pages, null, 2));
         return this.file;
     }
 }

6ed76beae7684b7df6ca561d70ba2369a26a0026
Jason <jason@10layer.com>
2024-12-20 17:11:13 +0100

    Add chumks and IDs

--- packages/common/src/models/Conversation.model.ts
+++ packages/common/src/models/Conversation.model.ts
@@ -1,14 +1,32 @@
 import mongoose, { Schema, Document } from 'mongoose';
 import type { Conversation } from '@sourcery/common/types/Conversation.type';
 import type { Message } from '@sourcery/common/types/Message.type';
+import { TChunk } from '@sourcery/common/types/Chunks.type';
 if (mongoose.models.Conversation) {
     mongoose.deleteModel('Conversation');
 }
 
+const ChunkSchema = new Schema<TChunk & Document>({
+    _id: {
+        type: String,
+        default: () => new mongoose.Types.ObjectId().toString()
+    },
+    title: String,
+    level: Number,
+    content: String,
+    parent: String,
+    context: String
+});
+
 const MessageSchema = new Schema<Message & Document>({
+    _id: {
+        type: String,
+        default: () => new mongoose.Types.ObjectId().toString()
+    },
     role: String,
     content: String,
     files: [{ type: Schema.Types.ObjectId, ref: 'File' }],
+    chunks: [ChunkSchema],
     created_at: { 
         type: Date, 
         default: Date.now,

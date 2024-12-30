976750f6305c47a6147dfbd211c7642d5b81fb67
Jason <jason@10layer.com>
2024-12-30 12:07:47 +0100

    Entitites model

---
+++ packages/common/src/models/Entity.model.ts
@@ -0,0 +1,49 @@
+import mongoose, { Schema, Document } from 'mongoose';
+import { Entity } from '@sourcery/common/types/Entities.type';
+
+if (mongoose.models.Entity) {
+    mongoose.deleteModel('Entity');
+}
+
+const EntitySchema = new Schema<Entity & Document>({
+    _id: {
+        type: String,
+        default: () => new mongoose.Types.ObjectId().toString()
+    },
+    id: {
+        type: String,
+        index: true
+    },
+    chunk_ids: [{ type: Schema.Types.ObjectId, ref: 'Chunk', index: true }],
+    project_id: { type: Schema.Types.ObjectId, ref: 'Project', index: true },
+    type: {
+        type: String,
+        required: true,
+        index: true
+    },
+    value: {
+        type: String,
+        required: true
+    },
+    description: String,
+    aliases: [String],
+    created_at: { 
+        type: Date, 
+        default: Date.now,
+        index: true
+    },
+    updated_at: { 
+        type: Date, 
+        default: Date.now,
+        index: true
+    }
+});
+
+EntitySchema.pre('save', function(this: Entity & Document, next) {
+    this.updated_at = new Date();
+    next();
+});
+
+EntitySchema.index({ type: 1, value: 1, project_id: 1 }, { unique: true });
+
+export const EntityModel = mongoose.model<Entity & Document>('Entity', EntitySchema);
--- packages/common/types/Entities.type.ts
+++ packages/common/types/Entities.type.ts
@@ -1,5 +1,14 @@
+import mongoose from 'mongoose';
+
 export interface Entity {
+    _id?: string;
+    id?: string;
+    chunk_ids: [string | mongoose.Types.ObjectId];
+    project_id: string | mongoose.Types.ObjectId;
     type: string;
     value: string;
     description?: string;
+    aliases?: string[];
+    created_at?: Date;
+    updated_at?: Date;
 }

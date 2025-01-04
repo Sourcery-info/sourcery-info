706fee0633a0cb813ef4c5298888e463eab602fd
Jason <jason@10layer.com>
2025-01-04 07:45:07 +0100

    Entity page

---
+++ packages/sourcery-frontend/src/routes/(authed)/entity/[project_id]/[entity_id]/+page.server.ts
@@ -0,0 +1,28 @@
+/** @type {import('./$types').PageServerLoad} */
+import { getEntity } from '$lib/classes/entities';
+import { getChunk } from '$lib/classes/chunks';
+
+import { error } from '@sveltejs/kit';
+import type { TChunk } from '@sourcery/common/types/Chunks.type';
+export async function load({ params }) {
+	const entity_id = params.entity_id;
+	let entity;
+	try {
+		entity = await getEntity(entity_id);
+	} catch (e) {
+		console.error(e);
+		throw error(404, 'Entity not found');
+	}
+	if (!entity) {
+		throw error(404, 'Entity not found');
+	}
+	let chunks: TChunk[] = [];
+	for (const chunk_id of entity?.chunk_ids) {
+		const chunk = await getChunk(chunk_id);
+		if (chunk) chunks.push(chunk);	
+	}
+	return {
+		entity,
+		chunks
+	};
+}
---
+++ packages/sourcery-frontend/src/routes/(authed)/entity/[project_id]/[entity_id]/+page.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	// @ts-nocheck
+	/** @type {import('./$types').PageData} */
+	import { goto } from '$app/navigation';
+	import { enhance } from '$app/forms';
+	import { entitiesStore } from '$lib/stores/entities';
+	export let data;
+	// Add tab state management
+</script>
+
+<div class="text-white">
+	<h1 class="text-3xl font-bold mb-4">Entity: {data.entity.value}</h1>
+	<div class="flex flex-col gap-4">
+		{#each data.chunks as chunk}
+			<div class="bg-gray-800 rounded-lg p-4 shadow-lg">
+				<div class="text-md text-gray-300 mb-2">{chunk.content}</div>
+				<div class="text-xl text-white">{chunk.context}</div>
+			</div>
+		{/each}
+	</div>
+</div>

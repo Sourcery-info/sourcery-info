e719c52dd5d0ab7e5c41925f72d825596e1e70cb
Jason <jason@10layer.com>
2025-01-06 10:19:26 +0100

    - Use chat history - Use active files - Allow for specifying temp, model, files, top-k

--- packages/sourcery-chat-api/src/app.js
+++ packages/sourcery-chat-api/src/app.js
@@ -5,11 +5,12 @@ import bodyParser from 'body-parser';
 import { Qdrant } from "@sourcery/sourcery-db/src/qdrant.ts";
 import { getProject } from "@sourcery/frontend/src/lib/classes/projects.ts";
 import { getFiles } from "@sourcery/frontend/src/lib/classes/files.ts";
+import { getConversation } from "@sourcery/frontend/src/lib/classes/conversations.ts";
 import { connectDB } from '@sourcery/frontend/src/lib/server/db';
 import dotenv from "dotenv";
 import { rerank } from "@sourcery/common/src/reranker";
 import { ensure_model } from "@sourcery/common/src/ollama";
-
+import { FileStatus } from "@sourcery/common/types/SourceryFile.type";
 dotenv.config();
 
 export const httpServer = restify.createServer();
@@ -26,18 +27,19 @@ httpServer.get("/", async () => {
     return { status: "ok" };
 });
 
-const rag_prompt_template = (context, question) => [
+const rag_prompt_template = (context, question, message_id, conversation) => [
     {
         role: "system",
         content: "You are named Sourcery, an AI that assists investigative journalists, able to find the most interesting facts and important information buried in boring documents. You will be given multiple chunks of information from different documents. Each chunk contains a filename, the context, and the relevant content. Using the information contained in the chunks, give a comprehensive answer to the question. Respond only to the question asked. The response should be concise and relevant to the question. Provide the filename of the source document when relevant. Quote the text from the document when relevant. Give as a complete answer as possible. If the answer cannot be deduced from the context, politely decline to give an answer. Answer in Markdown format."
     },
+    ...conversation.messages.filter(m => m._id !== message_id).map(m => ({ role: m.role, content: m.content })),
     {
         role: "user",
         content: `Context:\n${context}\n---\nHere is the question you need to answer.\n\nQuestion: ${question}`
     },
 ];
 
-const get_rag_context = async (project_name, files, question, top_k = TOP_K) => {
+const get_rag_context = async (project_name, files, question, top_k = TOP_K, vector_model = VECTOR_MODEL) => {
     const qdrant = new Qdrant({
         url: process.env.QDRANT_URL || "http://localhost:6333",
     });
@@ -60,15 +62,14 @@ const get_rag_context = async (project_name, files, question, top_k = TOP_K) => 
         return parent_results;
     }
     try {
-        await ensure_model(VECTOR_MODEL);
-        const vector = await ollama.embeddings({ model: VECTOR_MODEL, prompt: question });
+        await ensure_model(vector_model);
+        const vector = await ollama.embeddings({ model: vector_model, prompt: question });
         const file_query = {
             key: "filename",
             match: {
                 any: files.map(f => f.filename),
             }
         }
-
         const query = {
             top: top_k,
             vectors: vector.embedding,
@@ -94,55 +95,72 @@ const get_rag_context = async (project_name, files, question, top_k = TOP_K) => 
 }
 
 httpServer.post("/chat/:project_id", async (req, res) => {
-    const project_id = req.params.project_id;
-    if (!project_id) {
-        throw new restifyErrors.BadRequestError("No project provided");
-    }
-    if (!req.body?.input) {
-        throw new restifyErrors.BadRequestError("No input provided");
-    }
-    const project = await getProject(project_id);
-    if (!project?._id) {
-        throw new restifyErrors.NotFoundError("Project not found");
-    }
-    const message_id = req.body?.message_id;
-    if (!message_id) {
-        throw new restifyErrors.BadRequestError("No message_id provided");
-    }
-    let files = [];
-    if (req.body?.files) {
-        files = await getFiles(project_id, req.body.files);
-    } else {
-        files = await getFiles(project_id);
-    }
-    const { input } = req.body;
-    const context = await get_rag_context(project_id, files, input);
-    const messages = rag_prompt_template(context, input);
     try {
-        await ensure_model(MODEL);
-    } catch (err) {
-        console.error(err);
-        throw new restifyErrors.InternalServerError("Error processing chat");
-    }
-    const chatStream = await ollama.chat({ model: MODEL, messages, stream: true, options: { temperature: TEMPERATURE } }).catch(err => {
-        console.error(err);
-        throw new restifyErrors.InternalServerError("Error processing chat");
-    });
-    const response_log = [];
-    try {
-        for await (const response of chatStream) {
-            res.write(response.message.content);
-            response_log.push(response.message.content);
-            if (response.done) {
-                return res.end();
-            }
+        const project_id = req.params.project_id;
+        const { input, conversation_id, message_id, files } = req.body;
+        // Make sure we have all the required fields
+        if (!project_id) {
+            throw new restifyErrors.BadRequestError("No project provided");
         }
+        if (!input) {
+            throw new restifyErrors.BadRequestError("No input provided");
+        }
+        if (!message_id) {
+            throw new restifyErrors.BadRequestError("No message_id provided");
+        }
+        if (!conversation_id) {
+            throw new restifyErrors.BadRequestError("No conversation_id provided");
+        }
+        // Get the project
+        const project = await getProject(project_id);
+        if (!project?._id) {
+            throw new restifyErrors.NotFoundError("Project not found");
+        }
+        const model = req.body?.model || project?.model || MODEL;
+        const temperature = req.body?.temperature || project?.temperature || TEMPERATURE;
+        const top_k = req.body?.top_k || project?.top_k || TOP_K;
+        const vector_model = req.body?.vector_model || project?.vector_model || VECTOR_MODEL;
+        // Get the files to search
+        let files_to_search = [];
+        if (files) {
+            files_to_search = await getFiles(project_id, files);
+        } else {
+            files_to_search = (await getFiles(project_id)).filter(f => f.status === FileStatus.ACTIVE);
+        }
+        // Get the context
+        const context = await get_rag_context(project_id, files_to_search, input, top_k, vector_model);
+        // Get the history of messages
+        const conversation = await getConversation(conversation_id);
+        try {
+            await ensure_model(model);
+        } catch (err) {
+            console.error(err);
+            throw new restifyErrors.InternalServerError("Error loading model");
+        }
+        const messages = rag_prompt_template(context, input, message_id, conversation);
+        const chatStream = await ollama.chat({ model, messages, stream: true, options: { temperature } }).catch(err => {
+            console.error(err);
+            throw new restifyErrors.InternalServerError("Error processing chat");
+        });
+        const response_log = [];
+        try {
+            for await (const response of chatStream) {
+                res.write(response.message.content);
+                response_log.push(response.message.content);
+                if (response.done) {
+                    return res.end();
+                }
+            }
 
+        } catch (error) {
+            console.error(error);
+            new restifyErrors.InternalServerError("Error processing chat");
+        }
+        res.end();
     } catch (error) {
         console.error(error);
-        new restifyErrors.InternalServerError("Error processing chat");
+        throw new restifyErrors.InternalServerError("Error processing chat");
     }
-    res.end();
 })
 
 httpServer.get("/test", async (req, res) => {

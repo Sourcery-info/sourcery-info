655884a0af6fc25c889e57d16f34de66c04fd96c
Jason <jason@10layer.com>
2025-01-12 08:08:44 +0100

    Use "origin"

--- packages/sourcery-frontend/tests/test.js
+++ packages/sourcery-frontend/tests/test.js
@@ -1,6 +1,15 @@
 import { expect, test } from '@playwright/test';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const origin = process.env.ORIGIN;
+
+if (!origin) {
+	throw new Error('ORIGIN is not set');
+}
 
 test('index page has expected h1', async ({ page }) => {
-	await page.goto('/');
+	await page.goto(origin);
 	await expect(page.getByRole('heading', { name: 'Sourcery.info' })).toBeVisible();
 });
